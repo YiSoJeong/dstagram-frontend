37a8104f267fae3ae1ed8fa0c4e2f1075d950c78
Yi SoJeong <soj980312@gmail.com>
2020-07-17 02:13:52 +0900

    PostCard 컴포넌트 생성

---
+++ src/components/PostCard.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 600
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  avatar: {
+    backgroundColor: red[500]
+  }
+}));
+
+export default function PostCard() {
+  const classes = useStyles();
+
+  return (
+    <Grid item>
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={<Avatar className={classes.avatar}>R</Avatar>}
+          title="nickname"
+        />
+        <CardMedia className={classes.media} image="/.jpg" title="" />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            게시글 내용이다. 나는 할 수 있다! 우리는 할 수 있다! 종강해서 기분이
+            좋다. 놀고 싶은데 못 논다ㅠ
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+}
