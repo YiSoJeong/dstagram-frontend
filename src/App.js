431489315ec3f1e01bda154679b86269799d6e04
Yi SoJeong <soj980312@gmail.com>
2020-07-17 00:04:13 +0900

    전체 화면 크기 CSS 지정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Login from "./components/Login";
+import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: 50
+  },
+  paper: {
+    height: 850,
+    margin: theme.spacing(2),
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  },
+  rowfield: {
+    width: 300
+  }
+}));
 
 function App() {
-  return <div className="App"></div>;
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root} display="flex" justifyContent="center">
+      <Grid item xs={6} display="flex" justifyContent="center">
+        <Paper className={classes.paper} elevation={3}>
+          <Login />
+        </Paper>
+      </Grid>
+    </Box>
+  );
 }
 
 export default App;
