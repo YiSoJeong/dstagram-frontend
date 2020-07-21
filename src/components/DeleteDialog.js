35faa83994fba5ebcc704994b6d799a83a5ee04f
Yi SoJeong <soj980312@gmail.com>
2020-07-22 03:43:59 +0900

    DeleteDialog 수정
    취소 확인 버튼 작동 활성화

--- src/components/DeleteDialog.js
+++ src/components/DeleteDialog.js
@@ -1,25 +1,32 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogActions from "@material-ui/core/DialogActions";
+import {
+  Grid,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogActions
+} from "@material-ui/core";
 
 export default function DeleteDialog(props) {
   const { open, handleClose } = props;
 
+  const onClickDelete = () => {
+    handleClose();
+  };
+
   return (
-    <div>
+    <Grid>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>정말 삭제하시겠습니까?</DialogTitle>
         <DialogActions>
           <Button variant="contained" color="secondary" onClick={handleClose}>
             취소
           </Button>
-          <Button variant="contained" color="secondary" onClick={handleClose}>
+          <Button variant="contained" color="secondary" onClick={onClickDelete}>
             확인
           </Button>
         </DialogActions>
       </Dialog>
-    </div>
+    </Grid>
   );
 }
