d3c3e0044051678585b969c550d8a416a48dbf8b
Yi SoJeong <soj980312@gmail.com>
2020-07-22 06:01:57 +0900

    AlertDialog 수정
    경고 문구 추가 및 스타일 변경

--- src/components/AlertDialog.js
+++ src/components/AlertDialog.js
@@ -1,24 +1,21 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogTitle from "@material-ui/core/DialogTitle";
+import { Button, Dialog, DialogTitle } from "@material-ui/core";
 
 export default function AlertDialog(props) {
-  const { open, handleClose } = props;
+  const { open, handleClose, info } = props;
 
   const message = {
     loginError: "사용자를 찾을 수 없습니다.",
-    existUser: "기존에 있는 사용자 입니다."
+    existUser: "기존에 있는 사용자 입니다.",
+    emptyInput: "모든 정보를 입력해 주세요."
   };
 
   return (
-    <div>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>{message["loginError"]}</DialogTitle>
-        <Button variant="contained" color="secondary" onClick={handleClose}>
-          확인
-        </Button>
-      </Dialog>
-    </div>
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>{message[info]}</DialogTitle>
+      <Button variant="contained" color="secondary" onClick={handleClose}>
+        확인
+      </Button>
+    </Dialog>
   );
 }
